cdf77682b83a1eb79f3b9d6cb0f00d6544534bb3
markus7017 <markus.michels@me.com>
2017-06-07 12:07:36 +0200

    Create androiden_control.sh
    # AndroidTV_Control
    A bash script providing a bunch of functions to control your TV based on ADB.
    
    This script provides a rich set of functions allowing to control your Android TV (and other Android based devices like the amazon Fire TV stick - may also work with other Android based device types) .
    Basicly it uses ADB to send commands to the TV. In case of using it on a Raspberry Pi this means that you have to get and compile ADB for ARM. You also need to enable ADB connections on the TV in the settings menu (developer settings, e.g. FireTV) or running the appropirate java app. Check the Internet for more details. You should set the option to remember the pairing, otherwise you need to allow the connect each time you use the script. A new pairing is required any time you change the "calling device".
    The target scenario is the integration with openHAB - a smart home gateway, but could also be integrated in any other scenario. I use it to switch on/off the TV, select channels, start NETFLIX etc.
    
    You need to change the script code to reflect your IP setup:
    tv_ip="192.168.xx.xx"       # IP address of the TV (device id 1)
    tv_mac="xx:xx:xx:xx:xx:xx"  # MAC address of the TV (required to wake-on-lan"
    fire_tv_ip="192.168.xx.xx"  # IP address of the Fire TV (device id 2)
    The MAC address can be obtained from the TVs network settins/status
    
    NETFIX, TV Guide and Videotext are implemented by starting the appropirate App using ADB (thanks Paul for providing the information).
    
    Depending on the TV model you may need to adjust the way the TV is powered on and off. My requires a sleep and not pressing the power button. This ensures that the TV is switched on when its OFF and switches off when its ON. Using the power button as a toggle will fail once you used the remote to do it manually and openHAB doesn't get informed on that.
    
    Please note:
    - The script reflects my scenario and doesn't have the target to be a universal implementation supporting all types of Android TVs. Is was tricky to get everything running, esp. the fact that the TV is accepting the ADB connection.
    - I'm not a bash expert, so maybe some optimizations and more specific error handling could be supplied.
    
    Looking forward to any contribution. I could also provide some scripts for waking up a Apple-TV or controlling the Telekom Entertain Receiver. If you are interested feel free to cantact me.
    
    You could follow up the following thread in the openHAB community:
    https://community.openhab.org/t/philips-android-tv/15267/25
    
    HappySmartHoming & have fun,
    Markus
    
    
    Usage: tv_control.sh <command> [<device id>[log | trace | debug]]
    
    Valid commands:
    	status:  return device status - ON/OFF
    	on:      switch device ON
    	off:     switch device OFF
    	sleep:   switch device to sleep mode (you have to check if sleep or soft_sleep works with your device)
    	suspend: switch device to soft-sleep mode (you have to check if sleep or soft_sleep works with your device)
    	resume:  (try to) wakeup device (doesn't work if device is powered off)
    	ping:    keep network interface device online - prevent power down while sleeping
    	avr:     speical: switch to HDMI1 on Philips TV to select AVR
    
    	netflix: Open the NetFlix App
    	tvguide: Open the TV Guide App
    	videotext: Open the Videotext App
    
    	see script code for key commands (simulate remote keys)
    
    Device IDs:"
    	1: TV@$tv_ip:$port
    	2: Fire-TV@$fire_tv_ip:$port
    Default IP=$tv_ip; Default Port=$port (see script)
    
    Debugging":
    log      Enable logging to stdout
    trace    Enable ADB trace (output will be redirected to logfile)
    debug    Enable ADB debugging (output will be redirected to logfile)
    
    Examples:
    sudo ./tv_control.sh on (uses default IP $tv_ip - see script)
    sudo ./tv_control.sh home (send HOME key to device)
    sudo ./tv_control.sh sleep 192.168.1.1
    
    Note:
    Logfile: $log
    The scripts writes log messages to $log - change $$log in script if you don't have privileges there

---
+++ androiden_control.sh
@@ -0,0 +1,358 @@
+#!/bin/bash
+#
+# requires
+# - compiled version of adb under /usr/bin
+# - apt-get install etherwake
+#
+function e {
+	echo $1 >> $log
+}  
+
+function adb_start {
+	e Starting ADB Daemon...
+	adb start-server >> $log
+}
+
+function adb_kill {
+	adb kill-server >> $log
+	e "ADB Daemon killed."
+}
+
+function adb_connect {
+	if sudo adb devices | grep -q $uri; then
+		e "Already connected to $uri"
+	else
+		# Connect to device, if failed exit script
+		e "Connecting to $uri"
+		#adb_kill
+		adb connect $uri >> $log
+		device_status
+		if [ "$dev_status" == "ON" ]
+		then
+			sleep 2 &> /dev/null		# for timing
+			display_status
+		fi
+	fi
+}
+
+
+# device_status: returns the device/connection status into dev_status ("ON"/"OFF")
+function device_status {
+	e "Check device status for $uri..."
+  	if adb devices | grep -q $uri; then
+  		dev_status="ON"
+      else
+		dev_status="OFF"
+	fi
+	e "Device Status=$dev_status"
+}
+
+function display_status {
+	e "Check display status for $uri..."
+	if adb -s $uri shell dumpsys power | grep -q 'Display Power: state=ON'; then
+		dsp_status="ON"
+    else
+    	dsp_status="OFF"
+    fi
+	e "Display Status=$dsp_status"
+}
+
+function sendkey {
+	e "Send Key $1"
+	adb -s $uri shell input keyevent $1 >> $log
+}
+
+function tv_on {
+   	device_status
+   	display_status
+	if [ "$dev_status" == "OFF" ]
+	then
+		#wakeup the device
+		wakeonlan $tv_mac
+		until ping -c1 $tv_ip &> $log; do :; done
+		e "sleep 5sec"
+		sleep 5
+		adb_connect
+		sendkey 26		# Power Button
+   		device_status
+	   	display_status
+	fi
+	if [ "$dev_status" == "OFF" ]
+	then
+		sendkey 26		# Power Button
+	else
+		sendkey 170		# TV Button
+	fi
+}
+
+function presskey {
+local keycode
+
+	e "Press key '$1'"
+	key1=$(($1 / 10))
+	key2=$(($1 % 10))
+	e "Key1='$key1', Key2='$key2'"
+	if [ $key1 -gt 0 ]; then
+	case "$key1" in
+		"0") keycode=7;;
+		"1") keycode=8;;
+		"2") keycode=9;;
+		"3") keycode=10;;
+		"4") keycode=11;;
+		"5") keycode=12;;
+		"6") keycode=13;;
+		"7") keycode=14;;
+		"8") keycode=15;;
+		"9") keycode=16;;
+	esac
+	sendkey $keycode
+	fi
+
+	case "$key2" in
+		"0") keycode=7;;
+		"1") keycode=8;;
+		"2") keycode=9;;
+		"3") keycode=10;;
+		"4") keycode=11;;
+		"5") keycode=12;;
+		"6") keycode=13;;
+		"7") keycode=14;;
+		"8") keycode=15;;
+		"9") keycode=16;;
+	esac
+	sendkey $keycode
+}
+
+function usage {
+	echo
+	echo "Usage: tv_control.sh <command> [<device id>[log | trace | debug]]"
+	echo
+	echo "Valid commands:"
+	echo "	status:  return device status - ON/OFF"
+	echo "	on:      switch device ON"
+	echo "	off:     switch device OFF"
+	echo "	sleep:   switch device to sleep mode (you have to check if sleep or soft_sleep works with your device)"
+	echo "	suspend: switch device to soft-sleep mode (you have to check if sleep or soft_sleep works with your device)"
+	echo "	resume:  (try to) wakeup device (doesn't work if device is powered off)"
+	echo "	ping:    keep network interface device online - prevent power down while sleeping"
+	echo "	avr:     speical: switch to HDMI1 on Philips TV to select AVR"
+	echo
+	echo "	netflix: Open the NetFlix App"
+	echo "	tvguide: Open the TV Guide App"
+	echo "	videotext: Open the Videotext App"
+	echo
+	echo "	see script code for key commands (simulate remote keys)"
+	echo
+	echo "Device IDs:"
+	echo "	1: TV@$tv_ip:$port"
+	echo "	2: Fire-TV@$fire_tv_ip:$port"
+	echo "	Default IP=$tv_ip; Default Port=$port (see script)"
+	echo
+	echo "Debugging":
+	echo "	log      Enable logging to stdout"
+	echo "	trace    Enable ADB trace (output will be redirected to logfile)"
+	echo "	debug    Enable ADB debugging (output will be redirected to logfile)"
+	echo
+	echo "Examples:"
+	echo "	sudo ./tv_control.sh on (uses default IP $tv_ip - see script)"
+	echo "	sudo ./tv_control.sh home (send HOME key to device)"
+	echo "	sudo ./tv_control.sh sleep 192.168.1.1"
+	echo
+	echo "Note:"
+	echo "	Logfile: $log"
+	echo "	The scripts writes log messages to $log - change $$log in script if you don't have privileges there"
+	echo
+}
+
+#
+log_sw="no"   # no stdout
+log="/var/log/openhab2/tv_control.log"
+#log="/dev/stdout"
+#
+
+#
+# IP address and port for devices
+#
+tv_ip="192.168.6.20"       # IP address of the TV (device id 1)
+tv_mac="1C:5A:6B:A9:CC:4D"  # MAC address of the TV (required to wake-on-lan"
+fire_tv_ip="192.168.6.174"  # IP address of the Fire TV (device id 2)
+port="5555"
+dev_status="n/a"
+dsp_status="n/a"
+tv_key=""
+
+#export >> $log	# dump environment, e.g. user (privileges)
+
+if [ "$1" == "" ]
+then
+	usage
+	exit 1
+fi
+if [ "$1" == "--help" ]
+then
+	usage
+	exit 1
+fi
+
+
+#
+# tv_control
+#
+echo > $ log  # clear log
+
+cmd="$1"
+shift
+if [ "$cmd" == "presskey" ]
+then
+	e "Press key '$1'"
+	tv_key="$1"
+	shift
+fi
+
+# build URI from device ip and port (5555)
+case $1 in
+	"1") ip=$tv_ip;;          # TV
+	"2") ip=$fire_tv_ip;;     # Fire-TV
+	"")  ip=$tv_ip;;          # default = TV
+esac
+shift
+uri="$ip:$port"
+case $1 in
+	"log")    log="/dev/stdout"; log_sw="yes";;
+	"trace")  export ADB_TRACE=1;;
+	"debug")  export ADB_TRACE=1;;
+esac
+
+# If not yet connected start ADB server and connect to device
+# maybe it needs some more error handling
+e "TV Control (IP=$uri): $1"
+if [ "$cmd" == "check" ]
+then
+	device_status
+	display_status
+	echo "$dev_status"
+	exit
+fi
+adb_connect
+if [ "$device_status" == "OFF" ]
+then
+	e "Unable to connect to device."
+	e "ADB Log:"
+	if [ "$log_sw" == "yes" ]
+	then
+		cat "$log"
+	fi
+	exit 1
+fi
+
+#
+# process command
+#	
+ case "$cmd" in
+    "status")	# Display status
+    	display_status
+		echo "$dsp_status";;
+     
+    "on")
+      if adb -s $uri shell dumpsys power | grep -q 'Display Power: state=ON'; then
+        e "Display is already ON"
+      else
+        tv_on
+      fi
+      echo "ON";;
+
+    "off")
+      if adb -s $uri shell dumpsys power | grep -q 'Display Power: state=OFF'; then
+        e "Display is already OFF"
+      else
+        sendkey 26
+      fi
+      echo "OFF";;
+
+	"ping")
+		device_status
+		if [ "$dev_status" == "ON" ]
+		then
+		 	sendkey 223
+			echo "ON"
+		else
+			echo "OFF"
+		fi;;
+
+    "tv")
+		tv_on
+		dev_status
+		echo "$dev_status"
+    	;;
+
+	"avr")
+		tv_on
+		adb -s $uri shell "input keyevent 178 && input keyevent 122 && input keyevent 122 && input keyevent 20 && input keyevent 20 && input keyevent 20 && input keyevent 20 && input keyevent 20 && input keyevent 66";;
+	"netflix")
+		tv_on
+		adb -s $uri shell "am start -n com.netflix.ninja/com.netflix.ninja.MainActivity";;
+	"tvguide")
+		tv_on
+		adb -s $uri shell "am start -n org.droidtv.epg/org.droidtv.epg.bcepg.epgui.NonZiggo";;
+	"videotext")
+		tv_on
+		adb -s $uri shell "am start -n org.droidtv.teletext/org.droidtv.teletext.teletextfullscreen.TeletextFullscreen";;
+
+
+	"power")      sendkey 26;;		# Power Button
+	"sleep")      sendkey 223;;		# Sleep Button
+	"suspend")    sendkey 276;;		# Soft Sleep Button
+	"resume")     asendkey 224;;	# Wakeup Button	
+	"pairing")    sendkey 225;;		# Pairing Button
+	"settings")   sendkey 176;;		# Settings Button
+	"presskey")   presskey $tv_key;;
+
+    "input")      sendkey 178;;		# Source TV
+    "sat")        sendkey 237;;		# Source SAT
+    "hdmi1")      sendkey 243;;		# Source HDMI1
+    "hdmi2")      sendkey 244;;		# Source HDMI2
+    "hdmi3")      sendkey 245;;		# Source HDMI3
+    "hdmi4")      sendkey 246;;		# Source HDMI4
+    "composite1") sendkey 247;;		# Source Compsite1
+    "composite2") sendkey 248;;		# Source Compsite2
+    "component1") sendkey 249;;		# Source Component1
+    "component2") sendkey 250;;		# Source Component2
+    "vga")        sendkey 251;;		# Source VGA
+    "text")       sendkey 233;;		# Source Video-Text
+
+    "home")       sendkey 3;;		# Home Button
+    "menu")       sendkey 82;;		# Menu Button
+    "back")       sendkey 4;;		# Back Button
+    "top")        sendkey 122;;		# Top Button
+    "end")        sendkey 123;;		# End Button
+    "enter")      sendkey 66;;		# Enter Button
+    "up")         sendkey 19;;		# UP Button
+    "down")       sendkey 20;;		# DOWN Button
+    "left")       sendkey 21;;		# LEFT Button
+    "right")      sendkey 22;;		# RIGHT Button
+    "sysup")      sendkey 280;;		# SysUp Button
+    "sysdown")    sendkey 281;;		# SysDown Button
+    "sysleft")    sendkey 282;;		# SysLeft Button
+    "sysright")   sendkey 283;;		# SysRight Button
+  
+    "blue")       sendkey 186;;		# Blue Button
+    "green")      sendkey 184;;		# Green Button
+    "red")        sendkey183;;		# Red Button
+    "yellow")     sendkey 185;;		# Yellow Button
+    "move_home")  sendkey 122;;		# MoveHome Button
+    "search")     sendkey 84;;		# Search Button
+
+	"volup")      sendkey 24;;		# VolUp Button
+	"voldown")    sendkey 25;;		# VolDown Button
+	"mute")       sendkey 164;;		# Mute Button
+
+    "lastch")     sendkey 229;;		# LastChannel Button
+
+	"kill")       adb_kill;;		# Kill ADB
+	"start")      adb_start;;		# Start ADB
+
+	*)            echo "Unknown command: $cmd"; exit;;
+esac
+
+exit 0
+
