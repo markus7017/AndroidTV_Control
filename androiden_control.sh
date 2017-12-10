f95fea53b5f6bf9088538d3eb75030619282db98
Markus Michels <markus.michels@me.com>
2017-12-10 03:59:20 +0100

    Use cec-client to power on TV+HDMI channel for AVR
    The is a config var use_cec=1 to enable use of cec-client rather than adb to power on the TV and select the HDMI channel for the AVR. Also fixed some bugs and revised some code. See https://blog.gordonturner.com/2016/12/14/using-cec-client-on-a-raspberry-pi/ for information on cec-client

--- androiden_control.sh
+++ androiden_control.sh
@@ -2,8 +2,30 @@
 #
 # requires
 # - compiled version of adb under /usr/bin
-# - apt-get install etherwake
+# - apt-get install etherwake cec-utils
 #
+
+#
+log_sw="no"   # no stdout
+log="/usr/share/openhab2/log/tv_control.log"
+#log="/dev/stdout"
+#
+
+#
+# IP address and port for devices
+#
+tv_ip="192.168.6.20"        # IP address of the TV (device id 1)
+tv_mac="1C:5A:6B:A9:CC:4D"  # MAC address of the TV (required to wake-on-lan"
+fire_tv_ip="192.168.6.174"  # IP address of the Fire TV (device id 2)
+port="5555"                 # Port for ADB service
+use_cec=1                   # 1=use cec-client to control TV power on/off and HDMI channel select
+cec_port=RPI                # HDMI device to use, use "cec-client -l" to find, usually RPI for the Pi HDMI port (com port: RPI), /dev/ttyXXX for an HDMI USB controller
+hdmi_address_avr="10:00"    # HDMI device address, use sudo echo "scan" | cec-client -s -d 1 to find address, e.g. address "1.4.0.0" will be converted to 14:00
+dev_id="1"
+dev_status="n/a"
+dsp_status="n/a"
+tv_key=""
+
 function e {
 	echo $1 >> $log
 }  
@@ -39,11 +61,17 @@ function adb_connect {
 # device_status: returns the device/connection status into dev_status ("ON"/"OFF")
 function device_status {
 	e "Check device status for $uri..."
-  	if adb devices | grep -q $uri; then
-  		dev_status="ON"
-      else
-		dev_status="OFF"
-	fi
+	dev_status="OFF"
+#	if [ "$use_cec" == "1" ]; then
+#e "device_status: use_cec"
+#		if echo pow 0 | cec-client -s -d 1 | grep -q 'power status: on'; then
+#			dev_status="ON"
+#		fi
+#	else
+	  	if adb devices | grep -q $uri; then
+  			dev_status="ON"
+		fi
+#	fi
 	e "Device Status=$dev_status"
 }
 
@@ -58,69 +86,83 @@ function display_status {
 }
 
 function sendkey {
-	e "Send Key $1"
+	e "Send keycode=$1"
 	adb -s $uri shell input keyevent $1 >> $log
 }
 
-function tv_on {
-   	device_status
-   	display_status
-	if [ "$dev_status" == "OFF" ]
-	then
-		#wakeup the device
-		wakeonlan $tv_mac
-		until ping -c1 $tv_ip &> $log; do :; done
-		e "sleep 5sec"
-		sleep 5
-		adb_connect
-		sendkey 26		# Power Button
-   		device_status
-	   	display_status
-	fi
-	if [ "$dev_status" == "OFF" ]
-	then
-		sendkey 26		# Power Button
+function dev_on {
+	display_status
+	if [ "$dsp_status" == "OFF" ]; then
+		if [ "$dev_id" == "1" ]; then
+			#wakeup the device
+			wakeonlan $tv_mac
+			if [ "$use_cec" == "1" ]; then
+e "dev_on: use cec"
+				echo on 0 | cec-client $cec_port -s -d 1
+				sleep 2 #Adjust this if needed!
+				device_status
+				if [ "$dev_status" == "ON" ]; then
+					adb_connect
+				fi
+				tvservice -o   # disable HDMI
+			fi
+			until ping -c1 $tv_ip &> $log; do :; done
+			e "sleep 5sec"
+			sleep 5
+		else
+e "dev_on: use adb"
+			adb_connect
+			sendkey 26		# Power Button
+			sleep 2 #Adjust this if needed!
+			display_status
+			if [ "$dsp_status" == "OFF" ]; then
+				sendkey 26		# Power Button
+				dev_status
+				display_status
+			fi
+		fi
 	else
-		sendkey 170		# TV Button
+		e "Display is already ON"
 	fi
 }
 
-function presskey {
+function dev_off {
+	display_status
+	if [ "$dsp_status" == "OFF" ]; then
+		e "Display is already OFF"
+	else
+   	    sendkey 26		# Power button
+    fi
+}
+
+function send_button {
 local keycode
 
-	e "Press key '$1'"
-	key1=$(($1 / 10))
-	key2=$(($1 % 10))
-	e "Key1='$key1', Key2='$key2'"
-	if [ $key1 -gt 0 ]; then
-	case "$key1" in
-		"0") keycode=7;;
-		"1") keycode=8;;
-		"2") keycode=9;;
-		"3") keycode=10;;
-		"4") keycode=11;;
-		"5") keycode=12;;
-		"6") keycode=13;;
-		"7") keycode=14;;
-		"8") keycode=15;;
-		"9") keycode=16;;
+	case "$1" in
+		0) keycode=7;;
+		1) keycode=8;;
+		2) keycode=9;;
+		3) keycode=10;;
+		4) keycode=11;;
+		5) keycode=12;;
+		6) keycode=13;;
+		7) keycode=14;;
+		8) keycode=15;;
+		9) keycode=16;;
 	esac
 	sendkey $keycode
-	fi
+}
 
-	case "$key2" in
-		"0") keycode=7;;
-		"1") keycode=8;;
-		"2") keycode=9;;
-		"3") keycode=10;;
-		"4") keycode=11;;
-		"5") keycode=12;;
-		"6") keycode=13;;
-		"7") keycode=14;;
-		"8") keycode=15;;
-		"9") keycode=16;;
-	esac
-	sendkey $keycode
+function presskey {
+
+	e "Press key(s) '$1'"
+	key1=${1:0:1}
+	key2=${1:1:1}
+	key3=${1:2:1}
+	e "Key1='$key1', Key2='$key2', Key3='$key3'"
+	send_button $key1
+	send_button $key2
+	send_button $key3
 }
 
 function usage {
@@ -131,10 +173,10 @@ function usage {
 	echo "	status:  return device status - ON/OFF"
 	echo "	on:      switch device ON"
 	echo "	off:     switch device OFF"
+	echo "  presskey:send key code to device (code according Android key codes"
 	echo "	sleep:   switch device to sleep mode (you have to check if sleep or soft_sleep works with your device)"
 	echo "	suspend: switch device to soft-sleep mode (you have to check if sleep or soft_sleep works with your device)"
 	echo "	resume:  (try to) wakeup device (doesn't work if device is powered off)"
-	echo "	ping:    keep network interface device online - prevent power down while sleeping"
 	echo "	avr:     speical: switch to HDMI1 on Philips TV to select AVR"
 	echo
 	echo "	netflix: Open the NetFlix App"
@@ -164,22 +206,6 @@ function usage {
 	echo
 }
 
-#
-log_sw="no"   # no stdout
-log="/var/log/openhab2/tv_control.log"
-#log="/dev/stdout"
-#
-
-#
-# IP address and port for devices
-#
-tv_ip="192.168.6.20"       # IP address of the TV (device id 1)
-tv_mac="1C:5A:6B:A9:CC:4D"  # MAC address of the TV (required to wake-on-lan"
-fire_tv_ip="192.168.6.174"  # IP address of the Fire TV (device id 2)
-port="5555"
-dev_status="n/a"
-dsp_status="n/a"
-tv_key=""
 
 #export >> $log	# dump environment, e.g. user (privileges)
 
@@ -198,7 +224,7 @@ fi
 #
 # tv_control
 #
-echo > $ log  # clear log
+echo TV Control: $1 $2 $3> $log  # clear log
 
 cmd="$1"
 shift
@@ -210,7 +236,8 @@ then
 fi
 
 # build URI from device ip and port (5555)
-case $1 in
+dev_id="$1"
+case $dev_id in
 	"1") ip=$tv_ip;;          # TV
 	"2") ip=$fire_tv_ip;;     # Fire-TV
 	"")  ip=$tv_ip;;          # default = TV
@@ -248,61 +275,62 @@ fi
 #
 # process command
 #	
- case "$cmd" in
-    "status")	# Display status
+case "$cmd" in
+	"status")	# Display status
     	display_status
 		echo "$dsp_status";;
-     
-    "on")
-      if adb -s $uri shell dumpsys power | grep -q 'Display Power: state=ON'; then
-        e "Display is already ON"
-      else
-        tv_on
-      fi
-      echo "ON";;
 
-    "off")
-      if adb -s $uri shell dumpsys power | grep -q 'Display Power: state=OFF'; then
-        e "Display is already OFF"
-      else
-        sendkey 26
-      fi
-      echo "OFF";;
+	"on")
+		dev_on
+		device_status
+		echo "$dev_status";;
+
+	"off")
+		dev_off
+		echo "$dsp_status";;
+
+	"tv")
+		dev_on
+		sendkey 170		# TV Button
+		display_status
+		echo "$dsp_status";;
+
+	"avr")
+		dev_on
+		if [ "$use_cec" == "1" ]; then
+			e "Switch HDMI to address $hdmi_address_avr"
+			e "tx 2F:82:$hdmi_address_avr"
+			echo "tx 2F:82:$hdmi_address_avr" | cec-client $cec_port -s -d 1
+			echo "tx 2F:82:$hdmi_address_avr" | cec-client $cec_port -s -d 1
+		else
+			adb -s $uri shell "input keyevent 178 && input keyevent 122 && input keyevent 122 && input keyevent 122 && input keyevent 20 && input keyevent 20 && input keyevent 20 && input keyevent 20 && input keyevent 20 && input keyevent 66"
+		fi;;
 
 	"ping")
 		device_status
 		if [ "$dev_status" == "ON" ]
 		then
-		 	sendkey 223
+		 	sendkey 223  		# Sleep Button
 			echo "ON"
 		else
 			echo "OFF"
 		fi;;
 
-    "tv")
-		tv_on
-		dev_status
-		echo "$dev_status"
-    	;;
-
-	"avr")
-		tv_on
-		adb -s $uri shell "input keyevent 178 && input keyevent 122 && input keyevent 122 && input keyevent 20 && input keyevent 20 && input keyevent 20 && input keyevent 20 && input keyevent 20 && input keyevent 66";;
 	"netflix")
-		tv_on
+		dev_on
 		adb -s $uri shell "am start -n com.netflix.ninja/com.netflix.ninja.MainActivity";;
 	"tvguide")
-		tv_on
+		dev_on
 		adb -s $uri shell "am start -n org.droidtv.epg/org.droidtv.epg.bcepg.epgui.NonZiggo";;
 	"videotext")
-		tv_on
+		dev_on
 		adb -s $uri shell "am start -n org.droidtv.teletext/org.droidtv.teletext.teletextfullscreen.TeletextFullscreen";;
 
 
 	"power")      sendkey 26;;		# Power Button
 	"sleep")      sendkey 223;;		# Sleep Button
 	"suspend")    sendkey 276;;		# Soft Sleep Button
-	"resume")     sendkey 224;;		# Wakeup Button	
+	"resume")     sendkey 224;;     # Wakeup Button	
 	"pairing")    sendkey 225;;		# Pairing Button
 	"settings")   sendkey 176;;		# Settings Button
 	"presskey")   presskey $tv_key;;
